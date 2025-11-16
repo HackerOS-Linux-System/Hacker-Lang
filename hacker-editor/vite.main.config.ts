3fae15cec3cc4ae4f47bd64e9e75403d93d9036c
michal92299 <chudybyczek2137@gmail.com>
2025-11-16 10:17:05 +0100

    Create vite.main.config.ts

---
+++ hacker-editor/vite.main.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { join } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+    },
+  },
+  build: {
+    target: 'node20',
+  },
+});
