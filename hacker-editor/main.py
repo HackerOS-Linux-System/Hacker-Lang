c6b11ace62b5e131766ded285d1941c46637b62e
michal92299 <chudybyczek2137@gmail.com>
2025-11-18 12:47:15 +0100

    Update main.py

--- hacker-editor/main.py
+++ hacker-editor/main.py
@@ -1,1 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from editor import HackerEditor
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")  # Ładniejszy styl
+    editor = HackerEditor()
+    editor.show()
+    sys.exit(app.exec())
 
