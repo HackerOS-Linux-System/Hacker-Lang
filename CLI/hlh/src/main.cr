800aefb0caf843f5d0e43dcce302cf70b57f4207
michal92299 <chudybyczek2137@gmail.com>
2025-12-01 10:53:50 +0100

    Update main.cr

--- CLI/hlh/src/main.cr
+++ CLI/hlh/src/main.cr
@@ -1,1 +1,12 @@
+module Hlh
+  VERSION = "0.1.0"
 
+  def self.main
+    puts "Język programowania Hacker Lang dla HackerOS"
+    puts "bytes - manager bibliotek/pluginow"
+    puts "hli - narzędzie dla dużych projektów w .hacker wymaga plików bytes.yaml"
+    puts "hackerc - narzędzie do lekkich projektów i skryptów w .hacker nie wymaga bytes.yaml"
+  end
+end
+
+Hlh.main
