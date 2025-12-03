7de6b70132d81c1770ac82a14e0a46c99fa14722
michal92299 <chudybyczek2137@gmail.com>
2025-12-03 20:00:09 +0100

    Update main.cr

--- CLI/hlh/src/main.cr
+++ CLI/hlh/src/main.cr
@@ -1,51 +1,43 @@
-require "option_parser"
-require "colorize"
-
 module Hlh
   VERSION = "1.2"
-
   def self.display_info
-    puts "Język programowania Hacker Lang dla HackerOS".colorize(:cyan).bold
+    puts "\e[1;36mJęzyk programowania Hacker Lang dla HackerOS\e[0m"
     puts ""
-    puts "bytes".colorize(:green).bold + " - manager bibliotek/pluginów".colorize(:white)
-    puts "  Zarządza instalacją, aktualizacją i usuwaniem bibliotek oraz pluginów dla projektów w Hacker Lang."
-    puts "  Przykłady użycia:"
-    puts "    bytes install <pakiet>  - instaluje pakiet"
-    puts "    bytes update            - aktualizuje wszystkie pakiety"
+    puts "\e[1;32mbytes\e[0m \e[37m- manager bibliotek/pluginów\e[0m"
+    puts "\e[37m Zarządza instalacją, aktualizacją i usuwaniem bibliotek oraz pluginów dla projektów w Hacker Lang.\e[0m"
+    puts "\e[37m Przykłady użycia:\e[0m"
+    puts "\e[37m bytes install <pakiet> - instaluje pakiet\e[0m"
+    puts "\e[37m bytes update - aktualizuje wszystkie pakiety\e[0m"
     puts ""
-    puts "hli".colorize(:green).bold + " - narzędzie dla dużych projektów w .hacker wymaga plików bytes.yaml".colorize(:white)
-    puts "  Przeznaczone do zarządzania dużymi projektami, budowania, testowania i wdrażania."
-    puts "  Wymaga pliku konfiguracyjnego bytes.yaml definiującego zależności i ustawienia projektu."
-    puts "  Przykłady użycia:"
-    puts "    hli build               - buduje projekt"
-    puts "    hli run                 - uruchom projekt"
-    puts "    hli init                - tworzy przykładowy projekt projekt"
+    puts "\e[1;32mhli\e[0m \e[37m- narzędzie dla dużych projektów w .hacker wymaga plików bytes.yaml\e[0m"
+    puts "\e[37m Przeznaczone do zarządzania dużymi projektami, budowania, testowania i wdrażania.\e[0m"
+    puts "\e[37m Wymaga pliku konfiguracyjnego bytes.yaml definiującego zależności i ustawienia projektu.\e[0m"
+    puts "\e[37m Przykłady użycia:\e[0m"
+    puts "\e[37m hli build - buduje projekt\e[0m"
+    puts "\e[37m hli run - uruchom projekt\e[0m"
+    puts "\e[37m hli init - tworzy przykładowy projekt projekt\e[0m"
+    puts "\e[37m hli clean - czyści tymczasowe pliki\e[0m"
+    puts "\e[37m hli tutorials - przyklady\e[0m"
+    puts "\e[37m hli repl - używaj interaktywnego interfejsu\e[0m"
     puts ""
-    puts "hackerc".colorize(:green).bold + " - narzędzie do lekkich projektów i skryptów w .hacker nie wymaga bytes.yaml".colorize(:white)
-    puts "  Szybkie narzędzie do kompilacji i uruchamiania prostych skryptów lub małych projektów."
-    puts "  Nie wymaga konfiguracji, idealne do prototypowania i jednorazowych zadań."
-    puts "  Przykłady użycia:"
-    puts "    hackerc run <plik.hacker> - uruchamia skrypt"
-    puts "    hackerc compile <plik.hacker> - kompiluje do pliku wykonywalnego"
+    puts "\e[1;32mhackerc\e[0m \e[37m- narzędzie do lekkich projektów i skryptów w .hacker nie wymaga bytes.yaml\e[0m"
+    puts "\e[37m Szybkie narzędzie do kompilacji i uruchamiania prostych skryptów lub małych projektów.\e[0m"
+    puts "\e[37m Nie wymaga konfiguracji, idealne do prototypowania i jednorazowych zadań.\e[0m"
+    puts "\e[37m Przykłady użycia:\e[0m"
+    puts "\e[37m hackerc run <plik.hacker> - uruchamia skrypt\e[0m"
+    puts "\e[37m hackerc compile <plik.hacker> - kompiluje do pliku wykonywalnego\e[0m"
   end
-
   def self.main
-    OptionParser.parse do |parser|
-      parser.banner = "Użycie: hlh [opcje]"
-
-      parser.on("-v", "--version", "Wyświetla wersję") do
-        puts "hlh wersja #{VERSION}".colorize(:yellow)
-        exit
-      end
-
-      parser.on("-h", "--help", "Wyświetla pomoc") do
-        puts parser
-        exit
-      end
+    if ARGV.includes?("-v") || ARGV.includes?("--version")
+      puts "\e[33mhlh wersja #{VERSION}\e[0m"
+      exit
+    elsif ARGV.includes?("-h") || ARGV.includes?("--help")
+      puts "Użycie: hlh [opcje]"
+      puts "-v, --version    Wyświetla wersję"
+      puts "-h, --help       Wyświetla pomoc"
+      exit
     end
-
     display_info
   end
 end
-
 Hlh.main
