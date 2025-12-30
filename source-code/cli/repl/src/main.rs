3111ed3b7251cc3125f1871323c52ef3a058929a
michal92299 <chudybyczek2137@gmail.com>
2025-12-30 19:26:32 +0100

    Update main.rs

--- source-code/cli/repl/src/main.rs
+++ source-code/cli/repl/src/main.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 
 fn main() -> PyResult<()> {
-    let code = include_str!("./main.py");
+    let code = include_str!("../main.py");
     Python::with_gil(|py| {
         py.run_bound(code, None, None)
     })
