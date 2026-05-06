1e38513b849009f5209bab58282c59d2931421e1
michal92299 <chudybyczek2137@gmail.com>
2026-05-06 10:28:09 +0200

    Update lib.rs

--- source-code/parser/src/lib.rs
+++ source-code/parser/src/lib.rs
@@ -12,7 +12,6 @@ pub use lexer::{Lexer, Token, LexError};
 pub use parser::{Parser, ParseError, parse_source, parse_source_with_meta};
 pub use import_spec::{parse_import_line, ImportDecl};
 
-/// Wynik parsowania z pelna meta-informacja
 #[derive(Debug)]
 pub struct ParseMeta {
     pub nodes:   Vec<Node>,
